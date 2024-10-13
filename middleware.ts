4e28d5f5a7f5d6284a0881e461d610767e8e29d2
HS-JNYLee <2071013@hansung.ac.kr>
2024-10-13 15:23:08 +0900

    [feat]: middleware path filtering

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,29 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
+import {NextRequest, NextResponse} from "next/server";
 
 export default NextAuth(authConfig).auth
 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const session = request.cookies.get('user_session')
+
+  // List of paths that don't require authentication
+  const publicPaths = ['/login', '/register']
+
+  // Check if the requested path is public
+  const isPublicPath = publicPaths.some(path => request.nextUrl.pathname.startsWith(path))
+
+  if (!session && !isPublicPath) {
+    // Redirect to login page if there's no session and the path is not public
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  // Continue with the request if there's a session or if it's a public path
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 }
