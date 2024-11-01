627c473c330a0631cd0bba08d87fc104952db7b3
HS-JNYLee <2071013@hansung.ac.kr>
2024-11-02 08:34:17 +0900

    [ui]: FG-22 chatbot page command click focus textArea (optional)

--- components/button-command.tsx
+++ components/button-command.tsx
@@ -1,24 +1,36 @@
+import React, { Dispatch, SetStateAction, forwardRef } from 'react';
 import {Button, ButtonProps} from "@/components/ui/button";
-import {Dispatch, SetStateAction} from "react";
 
-interface ButtonCommand extends ButtonProps {
-    setInput: Dispatch<SetStateAction<string>>
-    command: string
+interface ButtonCommandProps extends ButtonProps {
+    setInput: Dispatch<SetStateAction<string>>;
+    command: string;
 }
 
 /**
  * 버튼 컴포넌트를 사용해 입력창에 명령어를 추가하는 컴포넌트
- * 
+ *
  * @param setInput 명령어를 치는 프롬프트 입력창에 set함수
  * @param command 입력할 명령어
+ * @param ref
  * @param props ButtonProps
  * @constructor
  */
-export function ButtonCommand({setInput, command, ...props}: ButtonCommand) {
-    return (
-        <Button variant="outline" size="sm" className="bg-white text-black rounded-full hover:bg-gray-100"
-                onClick={()=> setInput((prevState: string) => prevState + command)}>
-            {command}
-        </Button>
-    )
-}
+export const ButtonCommand = forwardRef<HTMLTextAreaElement, ButtonCommandProps>(
+    ({ setInput, command, ...props }, ref) => {
+
+        return (
+            <Button
+                variant="outline"
+                size="sm"
+                className="bg-white text-black rounded-full hover:bg-gray-100"
+                onClick={() => {
+                    setInput((prevState: string) => prevState + command);
+                    (ref as React.RefObject<HTMLTextAreaElement>).current?.focus(); // ref가 null이 아닐 때 focus
+                }}
+                {...props}
+            >
+                {command}
+            </Button>
+        );
+    }
+);
