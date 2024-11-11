2a9b954b822d467eb92d757c9fe292ee2b166e44
6-keem <6ukeem@gmail.com>
2024-11-11 14:51:47 +0900

    [refactor] router -> redirect

--- components/sign-up.tsx
+++ components/sign-up.tsx
@@ -118,7 +118,7 @@ export default function SignUpForm() {
                 path: '/'
               })
             }
-            redirect('http://hansung-fiveguys.duckdns.org')
+            redirect('http://hansung-fiveguys.duckdns.org/')
           } else {
             setError('오류가 발생했습니다.')
           }
