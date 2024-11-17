02d0499811959aca963b24af5b4312fc7246d4e2
황준익 <imbanana15@gmail.com>
2024-11-17 23:08:21 +0900

    m

--- app/edit/[imageid]/page.tsx
+++ app/edit/[imageid]/page.tsx
@@ -3,14 +3,11 @@ import ImageEditorForm from '@/components/image-editor'
 import { Session } from '@/lib/types'
 import { redirect } from 'next/navigation'
 interface EditPageProps {
-  params: Promise<{
-    imageid: string
-  }>
-  
+  params: Promise<{ imageid: string }>
 }
 export default async function EditPage({ params }: EditPageProps) {
-  const session = (await auth()) as Session
   const p = await params;
+  const session = (await auth()) as Session
 
   if (session) {
     redirect('/')
