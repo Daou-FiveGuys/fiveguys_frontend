87e0c214193c86ca18884dad7512553afe562335
junni01kim <audwnssp@naver.com>
2024-11-20 02:47:39 +0900

    /address 페이지 인자 추가

--- app/address/page.tsx
+++ app/address/page.tsx
@@ -1,11 +1,21 @@
 import AddressBook from './address-book'
+import { Contact2 } from './entity'
 import TopNav from './top-nav'
 
 export default function Home() {
+  /**
+   * 1. 추가
+   * 2. 수정
+   * 3. 삭제
+   * 4. 전체 삭제
+   */
+  const handleSelectContacts = (type: number, contact2: Contact2) => {
+  }
+
     return (
         <main className="container mx-auto p-4">
             <TopNav/>
-            <AddressBook/>
+            <AddressBook onSelectContacts={handleSelectContacts}/>
         </main>
     )
 }
