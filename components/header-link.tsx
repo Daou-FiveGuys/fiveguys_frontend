2567141bcf802a6ff91e67738910cd8b32236631
6-keem <6ukeem@gmail.com>
2024-11-21 00:38:17 +0900

    [bug] : remove "My Page" button in login & signup page

--- components/header-link.tsx
+++ components/header-link.tsx
@@ -6,22 +6,23 @@ import { Button } from '@/components/ui/button'
 import { IconNextChat, IconSeparator } from '@/components/ui/icons'
 
 export default function UserOrLogin() {
-  const pathname = usePathname() // 현재 경로를 가져옴
-
+  const pathname = usePathname()
   return (
     <>
       <Link href="/new" rel="nofollow">
         <IconNextChat className="size-6 mr-2 dark:hidden" inverted />
         <IconNextChat className="hidden size-6 mr-2 dark:block" />
       </Link>
-      {!pathname.includes('/mypage') && (
-        <div className="flex items-center">
-          <IconSeparator className="size-6 text-muted-foreground/50" />
-          <Button variant="link" asChild className="-ml-2">
-            <Link href="/mypage">My Page</Link>
-          </Button>
-        </div>
-      )}
+      {!pathname.includes('/mypage') &&
+        !pathname.includes('/login') &&
+        !pathname.includes('/signup') && (
+          <div className="flex items-center">
+            <IconSeparator className="size-6 text-muted-foreground/50" />
+            <Button variant="link" asChild className="-ml-2">
+              <Link href="/mypage">My Page</Link>
+            </Button>
+          </div>
+        )}
       {pathname.includes('/mypage') && (
         <div className="flex items-center">
           <IconSeparator className="size-6 text-muted-foreground/50" />
