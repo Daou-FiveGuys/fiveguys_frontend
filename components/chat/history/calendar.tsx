9a10e7a0da853b9a27e4285f8aab8fdfbfd22808
6-keem <6ukeem@gmail.com>
2024-11-23 00:08:07 +0900

    [bug-fix] : Error: Rendered more hooks than during the previous render

--- components/chat/history/calendar.tsx
+++ components/chat/history/calendar.tsx
@@ -41,10 +41,12 @@ export default function CalendarComponent({
     const newDate = new Date(
       currentDate.getFullYear(),
       currentDate.getMonth() - 1,
-      0
+      1
     )
     setCurrentDate(newDate)
-    setSelectedDate(newDate)
+    setSelectedDate(
+      new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0)
+    )
   }
 
   // 다음 달로 이동
@@ -52,10 +54,12 @@ export default function CalendarComponent({
     const newDate = new Date(
       currentDate.getFullYear(),
       currentDate.getMonth() + 1,
-      0
+      1
     )
     setCurrentDate(newDate)
-    setSelectedDate(newDate) // 새로운 달의 1일로 설정
+    setSelectedDate(
+      new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)
+    ) // 새로운 달의 1일로 설정
   }
 
   // 날짜 클릭 핸들러
