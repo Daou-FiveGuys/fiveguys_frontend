d118eda7fedc354bf17b07dca7ca7a117af3086a
junni01kim <audwnssp@naver.com>
2024-11-26 00:10:44 +0900

    DOM 에러 해결

--- components/chat/amount-used/amount-used-panel.tsx
+++ components/chat/amount-used/amount-used-panel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/redux/store'
 import { CSSTransition } from 'react-transition-group'
@@ -14,24 +14,35 @@ export interface SentMessages {
 }
 
 export default function AmountUsedPanel() {
-  const [visiable, setVisiable] = useState(false)
-  const isTyping = useSelector((state: RootState) => state.chat['amount-used'].isTyping)
+  const [visible, setVisible] = useState(false)
+  const isTyping = useSelector((state: RootState) => state.chat['amount-used']?.isTyping ?? false) // 안전한 접근
+  const nodeRef = useRef(null) // DOM 노드를 참조할 ref 생성
 
   useEffect(() => {
     if (!isTyping) {
-      setTimeout(() => {
-        setVisiable(true)
+      const timeout = setTimeout(() => {
+        setVisible(true)
       }, 60)
+
+      return () => clearTimeout(timeout) // 컴포넌트 언마운트 시 타이머 정리
+    } else {
+      setVisible(false)
     }
   }, [isTyping])
 
   return (
-    <CSSTransition in={visiable} timeout={1000} classNames="fade" unmountOnExit>
-      <div className="flex-col items-center justify-center mb-4">
+    <CSSTransition
+      in={visible}
+      timeout={1000}
+      classNames="fade"
+      unmountOnExit
+      nodeRef={nodeRef} // nodeRef 전달
+    >
+      <div ref={nodeRef} className="flex-col items-center justify-center mb-4">
         {/* TODO: 그래프 추가 */}
-        <AmountChart/>
+        <AmountChart />
         {/* TODO: 전체 정보 */}
-        <AmountUsedCard/>
+        <AmountUsedCard />
       </div>
     </CSSTransition>
   )
