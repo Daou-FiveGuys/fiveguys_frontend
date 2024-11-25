9402d65c503eb9bbac158dffc8f062a3f1aa48d9
junni01kim <audwnssp@naver.com>
2024-11-26 04:05:54 +0900

    주소록 모달창 뒤로 나오는 문제 해결

--- app/address/modal/page.tsx
+++ app/address/modal/page.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/dist/client/link'
+'use client'
+
 import AddressBookModal from './select-contact-modal'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+import { Input } from '@/components/ui/input'
 
 export default function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false)
@@ -11,10 +13,28 @@ export default function Home() {
         setFile(null)
       }
 
-
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0] || null
+        setFile(selectedFile)
+        if (selectedFile) {
+            setIsModalOpen(true) // 파일 선택 시 모달 열기
+        }
+    }
       
     return (
         <main className="container mx-auto p-4">
+            {/* ※ 임시코드!!!! 모달 적용하는 사람은 해당 정보 삭제할 것 */}
+            <label htmlFor="file-upload" className="cursor-pointer">
+                업로드할 파일 선택
+            </label>
+            <Input
+                id="file-upload"
+                type="file"
+                onChange={handleFileChange}
+                accept=".jpg, .jpeg"
+                className="hidden"
+            />
+
             {isModalOpen && file && (
                 <AddressBookModal
                     file={file}
